02f649d983da0586e7ef4d46c2b8b538825fbb79
Anna Dutko <dutko@ucu.edu.ua>
2017-04-07 23:28:12 +0300

    Delete Example_of_using_Catalog_class.py

--- examples/Example_of_using_Catalog_class.py
+++
@@ -1,9 +0,0 @@
-import udacity
-
-c = udacity.Catalog()
-
-tracks = c.tracks()
-
-track_names = [t['name'] for t in tracks]
-web_dev_teachers = c.instructors('cs253')
-nd001_description = c.degree('nd001')['expected_learning']
